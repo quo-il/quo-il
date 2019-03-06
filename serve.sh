2fe8d43328db1ee2ba552a0efe21dc6541a761ef
Igor Makarov <igormaka@gmail.com>
2019-03-06 21:01:02 +0200

    הפיכת טבלת הפוליטיקאים ליותר נוחה במובייל

--- serve.sh
+++ serve.sh
@@ -2,4 +2,4 @@
 
 ( sleep 2 ; open http://localhost:4000/ ) & &> /dev/null
 bundle exec filewatcher --immediate --restart '**/*.yml' \
-    'echo $FILENAME; bundle exec jekyll serve --port 4000'
+    'echo $FILENAME; bundle exec jekyll serve --port 4000 --host 0.0.0.0'
